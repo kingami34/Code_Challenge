18495ba84c32ba062b7b46775be610fc511b5053
Jeff Lynch <jlynch3@uw.edu>
2021-05-29 10:05:27 -0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var submitbutton= document.querySelector("#Submit")
+var quizquestion= document.querySelector("#quiz-question")
+var result= document.querySelector("#Result")
+var button= document.querySelector("#button")
+
+var questions = ["What is the meaning of HTML?","What is computer science","Which of these are most likely Not computing systems?","What is the difference between a consumer and a creator?", "What is a group of connected computer called?"]
+// Game starts when user click the "start here"
+submitbutton.addEventListener("click", function(){
+    //I to display first question and multiple choice answers
+    
+})
